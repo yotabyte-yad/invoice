083fe495166502184043c529230681a4995a41dc
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-06 23:50:24 +0530

    11/6/2015

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+
+///DB Configuration
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: "./book.sqlite"
+  }
+});
+
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.post('/createItem', function(req, res){
+	console.log(req.body);
+	knex.select().table('items')
+  .then(function(rows) {
+    console.log(rows);
+    res.json(rows);
+  })
+   .catch(function(error) {
+    console.error(error)
+  });
 });
 
 var server = app.listen(3000, function () {
