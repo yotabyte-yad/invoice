62916b4430f97ba4ca632504fb39fd51a810ee8d
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-07 00:44:34 +0530

    Serverjs is working
    app.post('/createItem', function(req, res){
    console.log(req.body);
    var pname = req.body.barcode;
    console.log('Printing Name' + pname);
    console.log(req.body.barcode);
    console.log(req.body.itemname);
    
    knex.select().table('items')
    .then(function(rows) {
    console.log(rows);
    res.json(rows);
    })
    .catch(function(error) {
    console.error(error)
    });
    
    });

--- server.js
+++ server.js
@@ -19,17 +19,40 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+  ///Extracting Item values in request body
+
 app.post('/createItem', function(req, res){
-	console.log(req.body);
-	knex.select().table('items')
-  .then(function(rows) {
-    console.log(rows);
-    res.json(rows);
-  })
-   .catch(function(error) {
-    console.error(error)
-  });
-});
+  console.log(req.body);
+  var pname = req.body.barcode;
+	console.log('Printing Name' + pname);
+  console.log(req.body.barcode);
+  console.log(req.body.itemname);
+  
+  
+  
+   knex.select().table('items')
+    .then(function(rows) {
+      console.log(rows);
+      res.json(rows);
+    })
+    .catch(function(error) {
+      console.error(error)
+    });
+  
+}); 
+
+// app.post('/createItem', function(req, res){
+// 	console.log("Request itemid " + req.body);
+    
+//     // knex.select().table('items')
+//     // .then(function(rows) {
+//     //   //console.log(rows);
+//     //   res.json(rows);
+//     // })
+//     // .catch(function(error) {
+//     //   console.error(error)
+//     // });
+// });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
