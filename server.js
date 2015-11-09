bd71cce60c742b9d98e013b8e23e6603e4a7981a
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-09 15:24:13 +0530

    Testing from UI for Anant ....no Functional changes

--- server.js
+++ server.js
@@ -140,6 +140,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at localhost', host, port);
 });
 
