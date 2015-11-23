e88da7d56dd3e321fc653f5254c8ec21c12e0687
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-23 20:27:30 +0530

    Success....posting data to server

--- public/views/items/items.js
+++ public/views/items/items.js
@@ -11,13 +11,12 @@ app.controller("ItemsCtrl", function ($location, $scope, $http, $rootScope){
 	// $scope.item.item_description = 'Enter comments';
 	
 	$scope.createItem = function(item){
-		console.log("within ItemsCtrl Items");	
-		console.log($scope.item);
-		$http.post('/item', $scope.item).success(function(response){
+		console.log("within ItemsCtrl Items", $scope.itemModel);
+		$http.post('/item', $scope.itemModel).success(function(response){
 			console.log('Server Response: ' );
 			console.log(response);
-			//$scope.serverResponse = response;
-				
+	}).error(function(e){
+		console.log('Error', e);
 	});
    };	
 });
