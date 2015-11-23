3f3b8eb38c10c2057237edbf9cb04d09a2f20aef
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-23 20:08:26 +0530

    Readying the Item Create Screen

--- public/views/items/items.js
+++ public/views/items/items.js
@@ -1,13 +1,14 @@
 //Test 
 app.controller("ItemsCtrl", function ($location, $scope, $http, $rootScope){
 	//console.log("clicked ItemsCtrl");
-	$scope.item = {};
-	$scope.item.item_name = 'Change';
-	$scope.item.item_mfg = 'Comp Change';
-	$scope.item.item_sch = 'N';
-	$scope.item.item_reorder_level = 0;
-	$scope.item.item_reorder_qty = 0;
-	$scope.item.item_description = 'Enter comments';
+	$scope.itemModel = {};
+	// $scope.item.item_name = 'Change';
+	// $scope.item.item_mfg = 'Comp Change';
+	// $scope.item.item_sch = 'N';
+	// $scope.item.item_tax_per = 0;
+	// $scope.item.item_reorder_level = 0;
+	// $scope.item.item_reorder_qty = 0;
+	// $scope.item.item_description = 'Enter comments';
 	
 	$scope.createItem = function(item){
 		console.log("within ItemsCtrl Items");	
