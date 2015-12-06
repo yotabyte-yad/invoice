202a90d8e99e549aff5b27b9aedf610d90fab0fa
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-12-06 14:01:01 +0530

    list and create seperation continued

--- public/views/mfgs/mfgs.js
+++ public/views/mfgs/mfgs.js
@@ -9,25 +9,5 @@ app.controller("MfgsCtrl", function ($location, $scope, $http, $rootScope){
 			console.log(response);
 		})
 	};
-
-	$scope.SearchData = function () {
-            var parameters = {
-                q: $scope.keyword
-            };
-            var config = {
-                params: parameters
-            };
-
-            $http.get('http://localhost:5000/mfgs', config)
-            .success(function (data, status, headers, config) {
-                $scope.Details = data;
-            })
-            .error(function (data, status, header, config) {
-                $scope.ResponseDetails = "Data: " + data +
-                    "<hr />status: " + status +
-                    "<hr />headers: " + header +
-                    "<hr />config: " + config;
-            });
-        };
 });	
 	
--- public/views/mfgs/mfgslist.js
+++ public/views/mfgs/mfgslist.js
@@ -1,4 +1,5 @@
 app.controller("MfgsListCtrl", function ($location, $scope, $http, $rootScope){
+	$scope.mfgsModel = {};
 
 	$scope.GetAllData = function () {
 	    $http.get('http://localhost:5000/mfgs')
@@ -11,10 +12,31 @@ app.controller("MfgsListCtrl", function ($location, $scope, $http, $rootScope){
 	            "<br />headers: " + jsonFilter(header) +
 	            "<br />config: " + jsonFilter(config);
 	    });
-	    console.log('got data');
-	    toastr.warning('Success, list is displayed');
+	    console.log($scope.allMfgs);
+	    toastr.success('Success, list is displayed');
 	};
 
 	$scope.GetAllData();
-		
+
+
+	$scope.SearchData = function () {
+            var parameters = {
+                q: $scope.keyword
+            };
+            var config = {
+                params: parameters
+            };
+
+            $http.get('http://localhost:5000/mfgs', config)
+            .success(function (data, status, headers, config) {
+                $scope.Details = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+        };
+
 });
