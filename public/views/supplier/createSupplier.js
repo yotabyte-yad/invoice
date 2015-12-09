54b02d9723e2f0b704d8bcc12200534c4bba2086
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-12-09 10:28:51 +0530

    Getting started with work on Supplier screen

--- public/views/supplier/createSupplier.js
+++ public/views/supplier/createSupplier.js
@@ -1,4 +1,6 @@
-//Test 
+//Test
+
+//Fields in model - name, tin, address, state, pincode, phone, person, email 
 app.controller("createSupplierCtrl", function ($location, $scope, $http, $rootScope){
 	$scope.supplierModel = {};
 	$scope.allMfgs = {};
